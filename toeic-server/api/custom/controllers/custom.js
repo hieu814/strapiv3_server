4632d7b13a95139b7dc47a96ffc7e1c739d0c20e
hieu <hieuvu81198@gmail.com>
2022-07-19 00:55:20 +0700

    modify model

--- toeic-server/api/custom/controllers/custom.js
+++ toeic-server/api/custom/controllers/custom.js
@@ -1,10 +1,15 @@
 'use strict';
 const _ = require('lodash');
-const { sanitizeEntity } = require('strapi-utils');
 
+const translateAPI = require('@vitalets/google-translate-api');
+const googleDictionaryApi = require("google-dictionary-api")
 const sanitizeUser = user =>
   sanitizeEntity(user, {
     model: strapi.query('user', 'users-permissions').model,
+  });
+  const sanitizePartType = part =>
+  sanitizeEntity(part, {
+    model: strapi.query('Part-Type').model,
   });
 
 module.exports = {
@@ -43,5 +48,50 @@ module.exports = {
     }
 
     return ctx.badRequest('New passwords do not match.');
-  }
+  },
+  async dictionary(ctx) {
+  
+    const params = _.assign({}, ctx.request.body);
+    if (
+      params.word
+    ) {
+
+    }
+    // ctx.send(ctx.request.body);
+    let entities = await googleDictionaryApi.search(params.word, 'en')
+    .then(results=>{
+      return results
+    })
+    .catch(error=>{
+      return error
+    })
+    return ctx.send(entities);
+
+   
+  },
+
+  async translate(ctx) {
+    const { word } = ctx.params;
+    if (
+      word
+    ) {
+      let entitiesss = await translateAPI(word, {to: 'vi'}).then(res => {
+        return res
+      }).catch(err => {
+        return err
+      });
+      return ctx.send(entitiesss.text);
+    }
+
+    return ctx.send("")
+  },
+
+  async getpart(ctx) {
+    const { type } = ctx.params;
+    const result = await strapi.query('Part').find({'part_type.type':type},);
+    
+    return result.map(entity => sanitizePart(entity));
+  },
+  
+  
 };
--- toeic-server/api/part/controllers/part.js
+++ toeic-server/api/part/controllers/part.js
@@ -4,5 +4,17 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+ const { sanitizeEntity } = require('strapi-utils');
+ const sanitizePart = part =>
+ sanitizeEntity(part, {
+   model: strapi.query('Part').model,
+ });
+module.exports = {
 
-module.exports = {};
+    async getpart(ctx) {
+        const { type } = ctx.params;
+        const result = await strapi.query('Part').find({'part_type.type':type},);
+        
+        return result.map(entity => sanitizePart(entity));
+      },
+};
